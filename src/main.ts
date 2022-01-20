31985d766f64901feaef2eee0df6efdd73e97b65
leodesa <leonardo.sa@paipe.co>
2022-01-19 22:24:37 -0300

    feat: port heroku

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './configs/winston.config';
+import env from './configs/env';
 
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(env().port);
 }
 bootstrap();
